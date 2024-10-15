9569e437191b976750590c0f6d3cd17ac12151fa
JanhaviJain <janhavijain0211@gmail.com>
2024-10-15 22:00:47 +0530

    packages

---
+++ the-mess-theory/src/App.js
@@ -0,0 +1,15 @@
+import './index.css';
+
+function App() {
+  return (
+    <>
+    <div className="bg-zinc-900 text-white inline">
+      hello
+    </div>
+    <h1 className='inline'>Hello</h1>
+    <Try>Hello</Try>
+    </>
+  );
+}
+
+export default App;
