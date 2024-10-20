9d028ce63df9c7c7c49e4b4b956b261b9f717723
JanhaviJain <janhavijain0211@gmail.com>
2024-10-20 18:41:39 +0530

    updated footer

--- the-mess-theory/src/components/Footer.jsx
+++ the-mess-theory/src/components/Footer.jsx
@@ -1,44 +1,119 @@
 import React from "react";
 // Initialization for ES Users
-const Footer = () => {
-    return(
-        <footer class="bg-zinc-800 text-white py-5">
-            <div class="sm:flex sm:justify-evenly">
-                <div class="flex flex-col items-center mb-4 sm:mb-0">
-                    <span class="bg-yellow-300 text-zinc-800 rounded-full p-1">Contact Us</span>
-                    <ul>
-                        <li>{/*Instagram*/}I</li>
-                        <li>{/*X*/}X</li>
-                        <li>{/*LinkdIn*/}M</li>
-                        <li>{/*xyz*/}X</li>
-                    </ul>
-                </div>
-                <div class="flex flex-col items-center">
-                <span class="bg-yellow-300 text-zinc-800 rounded-full p-1">Socials</span>
-                    <ul>
-                        <li>
-                     {/*   <span class="[&>svg]:h-5 [&>svg]:w-5">
-  look into this
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="current"
-    viewBox="0 0 320 512">
-  <path
-      d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
-  </svg>
-</span>*/}
-                        </li>
-                        <li></li>
-                        <li>{/*LinkdIn*/}M</li>
-                        <li>{/*xyz*/}X</li>
-                    </ul>
-                </div>
+export default function Footer () {
+    return (
+      <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8  text-zinc-300 bg-zinc-800">
+        <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
+          <div className="sm:col-span-2">
+              <span className="text-xl font-bold tracking-wide uppercase text-yellow-400">
+                The-Mess-Thoery
+              </span>
+            <div className="mt-6 lg:max-w-sm">
+                <p className="text-sm">
+                    The Mess Theory is dedicated to providing students with a seamless dining experience.
+                </p>
+                <br />
+                <p className="text-sm"> 
+                    Our platform allows you to manage meal selections, track meal history, and enjoy your time.
+                </p>
             </div>
-            <h1 class="text-yellow-300 text-2xl font-bold mb-1 text-center p-2 font-sans">The Mess Theory</h1>
-            <p class="text-center text-sm">
-                 © {new Date().getFullYear()} The Mess Theory. All rights reserved.
+          </div>
+          <div className="space-y-2 text-sm">
+            <p className="text-base font-bold tracking-wide text-yellow-400">
+              Contacts
             </p>
-        </footer>
-    )
-}
-export default Footer;
+            <div className="flex">
+              <p className="mr-1">Phone:</p>
+              <a
+                href="tel:8605769475"
+                aria-label="Our phone"
+                title="Our phone"
+                className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800"
+              >
+                (+91) 8605769475
+              </a>
+            </div>
+            <div className="flex">
+              <p className="mr-1">Email:</p>
+              <a
+                href="mailto:tmt@gmail.com"
+                aria-label="Our email"
+                title="Our email"
+                className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800"
+              >
+                tmt@gmail.com
+              </a>
+            </div>
+            <div className="flex">
+              <p className="mr-1">Address:</p>
+              <a
+                href="https://maps.app.goo.gl/qLa5yBTbcwoDvuoFA"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Our address"
+                title="Our address"
+                className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800"
+              >
+                JM Road, Shivajinagar, Pune
+              </a>
+            </div>
+          </div>
+          <div>
+            <span className="text-base font-bold tracking-wide text-yellow-400">
+              Social
+            </span>
+            <div className="flex items-center mt-1 space-x-3">
+              <a
+                href="/"
+                className=" transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                  <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
+                </svg>
+              </a>
+              <a
+                href="/"
+                className="transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
+                  <circle cx="15" cy="15" r="4" />
+                  <path d="M19.999,3h-10C6.14,3,3,6.141,3,10.001v10C3,23.86,6.141,27,10.001,27h10C23.86,27,27,23.859,27,19.999v-10   C27,6.14,23.859,3,19.999,3z M15,21c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S18.309,21,15,21z M22,9c-0.552,0-1-0.448-1-1   c0-0.552,0.448-1,1-1s1,0.448,1,1C23,8.552,22.552,9,22,9z" />
+                </svg>
+              </a>
+              <a
+                href="/"
+                className="transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                  <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
+          <p className="text-sm text-white">
+          © {new Date().getFullYear()} The-Mess-Theory. All rights reserved.
+          </p>
+          <ul className="flex flex-col  text-zinc-300 mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
+            <li>
+              <a
+                href="/"
+                className="text-sm transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="text-sm transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                Terms &amp; Conditions
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
